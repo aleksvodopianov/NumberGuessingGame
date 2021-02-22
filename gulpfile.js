72658a87b25dd5b837e480daa7a816ef77e7eb13
Aleks <vodopyanov3d@gmail.com>
2021-02-22 22:54:45 +0200

    modified

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,29 @@ let path = {
         js: sourceFolder + "/js/script.js",
         img: sourceFolder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
         fonts: sourceFolder + "/fonts/*.ttf",
-    }
+    },
+    watch: {
+        html: sourceFolder + "/**/*.html",
+        css: sourceFolder + "/sass/**/*.scss",
+        js: sourceFolder + "/js/**/*.js",
+        img: sourceFolder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+    },
+    clean: "./" + projectFolder + "/"
 };
+let { src, dist } = require('gulp'),
+    gulp = require('gulp'),
+    browsersync = require('browser-sync').create();
 
+function browserSync(params) {
+    browsersync.init({
+        server: {
+            baseDir: "./" + projectFolder + "/"
+        },
+        port: 3000,
+        notify: false,
+    });
+}
+
+let watch = gulp.parallel(browserSync);
+exports.watch = watch;
+exports.default = watch;
