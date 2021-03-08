9d521c0ad4e3e532aed5cd42e99830720d3edf7c
Aleks <vodopyanov3d@gmail.com>
2021-03-08 22:52:06 +0200

    created for desktop

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,96 @@
-function testWebP(callback) {
-    let webP = new Image();
-    webP.onload = webP.onerror = function () {
-        callback(webP.height == 2);
-    };
-    webP.src =
-        'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+// Next function to check browser support WEBP format;
+window.addEventListener('DOMContentLoaded', () => {
+    function testWebP(callback) {
+        let webP = new Image();
+        webP.onload = webP.onerror = function () {
+            callback(webP.height == 2);
+        };
+        webP.src =
+            'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+    }
+
+    testWebP(function (support) {
+        if (support == true) {
+            document.querySelector('body').classList.add('webp');
+            console.log('use webp');
+        } else {
+            document.querySelector('body').classList.add('no-webp');
+            console.log('not use webp');
+        }
+    });
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    let randomNumber = Math.floor(Math.random() * 100) + 1,
+    content = document.getElementById('content'),
+    guesses = document.querySelector('.guesses'),
+    lastResult = document.querySelector('.lastResult'),
+    lowOrHi = document.querySelector('.lowOrHi'),
+    guessSubmit = document.querySelector('.guessSubmit'),
+    guessField = document.querySelector('.guessField'),
+    guessCount = 1,
+    resetButton;
+
+function checkGuess() {
+    let userGuess = Number(guessField.value);
+    if (guessCount === 1) {
+        guesses.textContent = 'Previous guesses: ';
+    }
+    guesses.textContent += userGuess + ' ';
+
+    if (userGuess === randomNumber) {
+        lastResult.textContent = 'Congratulations! You got it right!';
+        lastResult.style.backgroundColor = 'green';
+        lowOrHi.textContent = '';
+        setGameOver();
+    } else if (guessCount === 10) {
+        lastResult.textContent = '!!!GAME OVER!!!';
+        setGameOver();
+    } else {
+        lastResult.textContent = 'Wrong!';
+        lastResult.style.backgroundColor = 'red';
+        if (userGuess < randomNumber) {
+            lowOrHi.textContent = 'Last guess was too low!';
+        } else if (userGuess > randomNumber) {
+            lowOrHi.textContent = 'Last guess was too high!';
+        }
+    }
+
+    guessCount++;
+    guessField.value = '';
+    guessField.focus();
 }
 
-testWebP(function (support) {
-    if (support == true) {
-        document.querySelector('body').classList.add('webp');
-        console.log('use webp');
-    } else {
-        document.querySelector('body').classList.add('no-webp');
-        console.log('not use webp');
+guessSubmit.addEventListener('click', checkGuess);
+
+function setGameOver() {
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Start new game';
+    content.appendChild(resetButton);
+    resetButton.style.marginTop = "10px";
+    resetButton.addEventListener('click', resetGame);
+}
+
+function resetGame() {
+    guessCount = 1;
+
+    let resetParas = document.querySelectorAll('.resultParas p');
+    for (let i = 0; i < resetParas.length; i++) {
+        resetParas[i].textContent = '';
     }
+
+    resetButton.parentNode.removeChild(resetButton);
+
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.value = '';
+    guessField.focus();
+
+    lastResult.style.backgroundColor = 'white';
+
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+}
 });
+
